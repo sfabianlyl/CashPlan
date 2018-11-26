f3addbf75f8ad9d45232bf0f2dc57683750c2045
sfabianlyl <36915473+sfabianlyl@users.noreply.github.com>
2018-11-26 22:44:37 +0800

    Delete dbconn.php

--- dbconn.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $db_username="root";
-    $db_password="";
-    $db_servername="localhost";
-    $db_dbname="cashplan";
-
-    $conn=new mysqli($db_servername,$db_username,$db_password,$db_dbname);
-?>
